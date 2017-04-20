fc99da8cad5c71bc7368e428967c6c15fd109a74
prakhars777 <prakhars777@gmail.com>
2017-04-21 02:32:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/prakhar/CollegeConnect/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.prakhar.CollegeConnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+
+
+
+        mWebView.loadUrl("http://collegeconnect.tech");
+        mWebView.setWebViewClient(new WebViewClient());
+    }
+}
